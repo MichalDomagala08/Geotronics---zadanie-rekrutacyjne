d0a4086bcca8519aae8823a6bf43a2493414f9e1
MichalDomagala08 <56306081+MichalDomagala08@users.noreply.github.com>
2019-10-23 01:42:32 +0200

    Update punkty_geog.sql

--- punkty_geog.sql
+++ punkty_geog.sql
@@ -1,5 +1,20 @@
+--Tworzenie Tabeli do której wpisuję punkty
 CREATE TABLE punkty_geom2
 (
     geom geometry(PointZ) Not null,
     id serial Primary key
 );
+
+--Tworzenie Bliźniaczej Tabeli punkty_geog2
+CREATE TABLE punkty_geog2 AS
+SELECT
+  Geography(ST_Transform(geom,4326)) AS geog,
+  id
+FROM punkty_geom2;
+                         
+--Tworzenie Bliźniaczej Tabeli punkty_geog3
+ CREATE TABLE punkty_geog3 AS
+SELECT
+  geog AS geog,
+  id
+FROM punkty_geog2;                        
