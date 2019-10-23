3db3421d115845de9ea659e9e26c3f3305f0d6ae
MichalDomagala08 <56306081+MichalDomagala08@users.noreply.github.com>
2019-10-23 10:28:19 +0200

    Update Program.cs
    Dodanie Funkcji sprawdzającej  czy każdy punkt jest odległy od każdego o 30km

--- Geotronics_AddRandomRecord_Spatial/Program.cs
+++ Geotronics_AddRandomRecord_Spatial/Program.cs
@@ -14,8 +14,9 @@ namespace Geotronics_AddRandomRecord_Spatial
                                     "Password=Qetchua08!;Database=Geotronics_Domagala;");
 
             conn.Open();
-            Is30kmFrom(conn/*,1*/);
-            // IsWithinWojewodztwo(conn, "swietokrzyskie");
+            //Is30kmFrom(conn);
+            // Console.WriteLine(IsAll30kmFrom2(conn));
+            // IsWithinWojewodztwo(conn);
             // AddRandomSpatialPoints(conn);
             conn.Close();
 
@@ -62,36 +63,81 @@ namespace Geotronics_AddRandomRecord_Spatial
         }
 
         //Czy w pdanym województwe (jeśli w ogóle) jest  punkt, oraz który to punkt
-        static void IsWithinWojewodztwo(NpgsqlConnection connection, string wojewodztwo)
+        static void IsWithinWojewodztwo(NpgsqlConnection connection)
         {
-            
-            Console.WriteLine("Proszę Wpisać nazwę Województwa bez Polskich znaków");
-            string d = Console.ReadLine();
-            Console.WriteLine();
-            NpgsqlCommand cmd3 = new NpgsqlCommand("SELECT ST_X(punkty_geom2.geom),ST_Y(punkty_geom2.geom) ,\"Województwa\".jpt_nazwa_ as woj FROM \"Województwa\" join punkty_geom2 on ST_Within(punkty_geom2.geom, ST_SetSRID(\"Województwa\".geom, 4326)) Where \"Województwa\".jpt_nazwa_ = @W", connection);
-            cmd3.Parameters.AddWithValue("W", d);
-            NpgsqlDataReader dr = cmd3.ExecuteReader();
-            Console.WriteLine(" X             Y            WOJ    ");
-            while (dr.Read())
-                Console.WriteLine("{0}    {1}    {2}    ", dr[0], dr[1], dr[2]);
+            //Reading Wojewodztwo from keyboard
+                Console.WriteLine("Proszę Wpisać nazwę Województwa bez Polskich znaków");
+                string d = Console.ReadLine();
+                Console.WriteLine();
+
+            //Querry with already red Wojewodztwo
+                NpgsqlCommand cmd3 = new NpgsqlCommand("SELECT ST_X(punkty_geom2.geom),ST_Y(punkty_geom2.geom) ,\"Województwa\".jpt_nazwa_ as woj " +
+                "FROM \"Województwa\" join punkty_geom2 on ST_Within(punkty_geom2.geom, ST_SetSRID(\"Województwa\".geom, 4326)) " +
+                "Where \"Województwa\".jpt_nazwa_ = @W", connection);
+                cmd3.Parameters.AddWithValue("W", d);
+
+            //Writing all Points in Selected Wojewodztwo
+                NpgsqlDataReader dr = cmd3.ExecuteReader();
+                Console.WriteLine(" X             Y            WOJ    ");
+                while (dr.Read())
+                    Console.WriteLine("{0}    {1}    {2}    ", dr[0], dr[1], dr[2]);
         }
-        //Czy Punkt o podanym ID punkt jest 30km od wszystkich
+        //Czy Dany punkt jest 30km od wszystkich
         static void Is30kmFrom(NpgsqlConnection connection/*,int ID*/)
         {
+            //Uzyskanie ID Punktu 
+                Console.WriteLine("Podaj ID punktu do sprawdzenia");
+                var z = Console.ReadLine();
+                int x = Int32.Parse(z);
 
-            Console.WriteLine("Podaj ID punktu do sprawdzenia");
-            var z = Console.ReadLine();
+            //Zapytanie oraz wczytanie ID
+                NpgsqlCommand cmd5 = new NpgsqlCommand("SELECT  punkty_geog3.id as \" point id\", (ST_Distance(punkty_geog2.geog, punkty_geog3.geog) <= 30000) " +
+                "as \"Is Within 30 km\" FROM punkty_geog2 JOIN punkty_geog3 ON ST_Area(punkty_geog3.geog) <= 0 where punkty_geog2.id = @D ", connection);
+                cmd5.Parameters.AddWithValue("D",x);
 
-            int x = Int32.Parse(z);
+            // Wypisanie Punktów
+                NpgsqlDataReader dr = cmd5.ExecuteReader();
+                Console.WriteLine("PointId     Is it within 30 km?    ");
+                while (dr.Read())
+                    Console.WriteLine("{0}             {1}    ", dr[0], dr[1]);
 
+        }
+        // Czy wszystkie punkty są 30km od Siebie? UWAGA! Mało prawdopodobne że przy niecelowo wpisanych punktach kiedykolwiek zwróci True
+        static string IsAll30kmFrom(NpgsqlConnection connection/*,int ID*/)
+        {
+            //licznik punktów
+                var cmd6 = new NpgsqlCommand("Select COUNT(*) FROM punkty_geog2",connection);
+            //Zwracanie Czy dany punkt jest w odl. 30 km od sebie
+                var cmd5 = new NpgsqlCommand("SELECT  punkty_geog3.id as \" point id\", (ST_Distance(punkty_geog2.geog, punkty_geog3.geog) <= 30000)" +
+                " as \"Is Within 30 km\" FROM punkty_geog2 JOIN punkty_geog3 ON ST_Area(punkty_geog3.geog) <= 0 where punkty_geog2.id = @D ", connection);
 
-            NpgsqlCommand cmd5 = new NpgsqlCommand("SELECT  punkty_geog3.id as \" point id\", (ST_Distance(punkty_geog2.geog, punkty_geog3.geog) <= 30000) as \"Is Within 30 km\" FROM punkty_geog2 JOIN punkty_geog3 ON ST_Area(punkty_geog3.geog) <= 0 where punkty_geog2.id = @D ", connection);
-            cmd5.Parameters.AddWithValue("D",x);
-            NpgsqlDataReader dr = cmd5.ExecuteReader();
-            Console.WriteLine("PointId     Is it within 30 km?    ");
-            while (dr.Read())
-                Console.WriteLine("{0}             {1}    ", dr[0], dr[1]);
+            //Odzczytanie Licznika oraz przypisanie go do int i
+                NpgsqlDataReader dr2 = cmd6.ExecuteReader();
+                dr2.Read();
+                int i = Convert.ToInt32(dr2[0]);
+                dr2.Close();
+                dr2.Dispose();
 
+            //Pętla iterująca przez wszystkie punkty, zwracająca TRUE - tylko jeśli któryś punkt nie jest FALSE
+                cmd5.Parameters.AddWithValue("D", 1);
+                NpgsqlDataReader dr = cmd5.ExecuteReader();
+            
+                for(int n = 0; n <i+1;n++)
+                {
+                    while (dr.Read())
+                    {
+                        cmd5.Parameters.AddWithValue("D",n+1);
+                        if (dr[1].ToString() == "True")
+                        {
+                            
+                        }
+                        else
+                        {
+                            return false.ToString();
+                        }
+                    }
+                };
+                return true.ToString();
         }
     }
 
